a58fdbe753c76d3bc5d9801cf9ec16ab56766415
damjanjokic <damjanmjokic@gmail.com>
2020-01-09 17:40:30 +0100

    Partially completed Banking Microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class BankingController : Controller
+    {
+        private readonly IAccountService _service;
+
+        public BankingController(IAccountService service)
+        {
+            _service = service;
+        }
+
+
+       [HttpGet]
+       public ActionResult<IEnumerable<Account>> GetAccounts()
+        {
+            return Ok(_service.GetAccounts());
+        }
+    }
+}
